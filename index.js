029cdba77c658742e8064a17740e905b3a29bd1a
purpesy <lucasgabrieldossantos753@gmail.com>
2025-05-30 20:21:05 -0300

    Fiz as rotas de adicionar e buscar categorias.

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ const app = express();
 const PORT = 8080;
 const cors = require("cors");
 const connection = require("./database/Conexao");
+
+// Importação dos models
 const Tarefas = require("./database/Tarefas");
 const Categoria = require("./database/Categoria");
 
+// Configuração de relação
+Categoria.hasMany(Tarefas, { foreignKey: "categoriaId" });
+Tarefas.belongsTo(Categoria, { foreignKey: "categoriaId" });
+
+// Conexão com o banco de dados
 connection.authenticate().then(() => {
   console.log("Conexão com o banco de dados realizada com sucesso!");
 }).catch((error) => {
   console.log("Erro ao conectar com o banco de dados: ", error);
 });
 
+// Configuração do express
 app.use(cors());
 app.use(express.json());
 
@@ -20,16 +28,18 @@ app.use(express.json());
 // Rotas para as tarefas
 // ==============================
 
+// Busca todas as tarefas com as categorias
 app.get("/tarefas", (req, res) => {
-  Tarefas.findAll().then((tarefas) => {
+  Tarefas.findAll({ include: Categoria }).then((tarefas) => {
     res.json(tarefas);
   });
 });
 
+// Cria uma nova tarefa
 app.post("/addtarefa", (req, res) => {
   const { titulo, descricao, status, categoriaId } = req.body;
 
-  if (titulo || descricao || status || categoriaId) {
+  if (titulo && descricao && status && categoriaId) {
   Tarefas.create({
     titulo,
     descricao,
@@ -43,11 +53,12 @@ app.post("/addtarefa", (req, res) => {
   }
 });
 
+// Atualiza uma tarefa existente
 app.put("/atualizartarefa/:id", (req, res) => {
   const { id } = req.params;
   const { titulo, descricao, status, categoriaId } = req.body;
 
-  if (titulo || descricao || status || categoriaId) {
+  if (titulo && descricao && status && categoriaId) {
     Tarefas.update({
       titulo,
       descricao,
@@ -64,12 +75,14 @@ app.put("/atualizartarefa/:id", (req, res) => {
   
 });
 
+// Deleta uma tarefa existente
 app.delete("/apagartarefa/:id", (req, res) => {
   const { id } = req.params;
   Tarefas.findByPk(id).then((tarefa) => {
     if (tarefa) {
-      tarefa.destroy();
-      res.status(200).json({ message: "Tarefa deletada com sucesso" });
+      tarefa.destroy().then(() => {
+        res.status(200).json({ message: "Tarefa deletada com sucesso" });
+      });
     } else {
       res.status(404).json({ message: "Tarefa não encontrada" });
     }
@@ -79,6 +92,31 @@ app.delete("/apagartarefa/:id", (req, res) => {
 // ==============================
 // Rotas para as categorias
 // ==============================
+
+// Busca todas as categorias
+app.get("/categorias", (req, res) => {
+  Categoria.findAll().then((categorias) => {
+    res.json(categorias);
+  });
+});
+
+// Cria uma nova categoria
+app.post("/addcategoria", (req, res) => {
+  const { nomeCategoria } = req.body;
+
+  if (nomeCategoria) {
+  Categoria.create({
+    nomeCategoria
+  }).then(() => {
+    res.status(201).json({ message: "Categoria criada com sucesso" });
+  });
+  } else{
+    res.status(400).json({ message: "Todos os campos são obrigatórios" });
+  }
+});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
